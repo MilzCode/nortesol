e4d26de630fff18c04cfb521da320256200c2195
sharshakap <55374102+sharshakap@users.noreply.github.com>
2021-09-11 04:02:19 -0300

    feat: comenzando filtro pagina search

--- components/search/Filtro.tsx
+++ components/search/Filtro.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Select from "react-select";
 
 const Filtro = () => {
-  return <div className="filtro">
-      
-  </div>;
+  const [selectedOption, setSelectedOption] = useState([]);
+  const marcas = [
+    { value: "torre", label: "Torre" },
+    { value: "murano", label: "Murano" },
+    { value: "colon", label: "Colon" },
+  ];
+  return (
+    <div className="filtro">
+      <div className="filtro__marca">
+        <Select
+          inputId="filtro-marca"
+          isMulti
+          name="marcas"
+          options={marcas}
+          className="basic-multi-select"
+          classNamePrefix="select"
+          value={selectedOption}
+          onChange={(e: any) => setSelectedOption(e)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Filtro;
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Volver from "../components/general/Volver";
+import Filtro from "../components/search/Filtro";
 const Search = () => {
   return (
     <div>
-
+      <Volver />
+      <Filtro />
     </div>
   );
 };
