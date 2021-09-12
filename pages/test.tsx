f3e0217ebc768402d90cc567a1e1b6efd432b2d3
sharshakap <55374102+sharshakap@users.noreply.github.com>
2021-09-11 21:02:47 -0300

    feat: NOUISLIDER SCSS
    next no me esta tomando el archivo que viene por defecto asi que lo creare aparte

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Nouislider from "nouislider-react";
 
 const Test = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <Nouislider range={{ min: 0, max: 100 }} start={[20, 80]} connect />
+    </div>
+  );
+};
 
-export default Test
+export default Test;
