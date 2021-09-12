718668e68a182e8b73da057204b01b75e08e4bea
sharshakap <55374102+sharshakap@users.noreply.github.com>
2021-09-12 19:03:32 -0300

    fix: slider terminado

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,30 +1,12 @@
 import React from "react";
 import Nouislider from "nouislider-react";
-
-//format 55555 to $55,555
-const formatNumberToprice = (value: number, maxValue: number) => {
-  // value = Math.floor(Math.log(value + 1) * (maxValue / Math.log(100 + 1)));
-  return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-};
+import SliderPrecios from "../components/venta/SliderPrecios";
 
 const Test = () => {
   return (
     <div className="test">
-      <h1>Slider</h1>
-      <Nouislider
-        range={{ min: 0, max: 100 }}
-        start={[0, 100]}
-        connect
-        tooltips={[true, true]}
-        format={{
-          to: (value: number) => {
-            return `$${formatNumberToprice(value, 5000000)}`;
-          },
-          from: (value: string) => {
-            return parseInt(value);
-          },
-        }}
-      />
+      <h1 className="mb-5">Slider</h1>
+      <SliderPrecios minValue={0} maxValue={200000} onChange={(e: any) => console.log(e)} />
     </div>
   );
 };
