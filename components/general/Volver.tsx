344b3ab7d528228298e164cc81e1d4dc600068ea
sharshakap <55374102+sharshakap@users.noreply.github.com>
2021-09-13 20:39:38 -0300

    fix: se añadio funcionalidad volver

--- components/general/Volver.tsx
+++ components/general/Volver.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const Volver = () => {
+  const router = useRouter();
   return (
-    <div className="volverBoton NOSELECT">
+    <div className="volverBoton NOSELECT" onClick={() => router.back()}>
       <i className="fas fa-level-up-alt" />
       <span>Volver</span>
     </div>
