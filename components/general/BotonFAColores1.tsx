0d566ebddbecedd953d7f6af2765d3cde2bdb8e5
sharshakap <55374102+sharshakap@users.noreply.github.com>
2021-09-14 02:24:32 -0300

    feat: se añadio componente boton

---
+++ components/general/BotonFAColores1.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+/*
+Este componente es un boton que puede recibir un icono de fontawesome como children
+  Recibe:
+  -children = icono de fontawesome (opcional) + texto
+  -backgroundColor = color de fondo del boton (por defecto: #ea5c90)
+  -className = clase extra (opcional)
+  -onClick = funcion que se ejecuta al hacer click (opcional)
+  -otras props de button
+*/
+
+const BotonFAColores1 = (
+  { children, backgroundColor, className, onClick }: any,
+  props: any
+) => {
+  return (
+    <button
+      className={`botonColores1 ${className}`}
+      style={{ backgroundColor }}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default BotonFAColores1;
