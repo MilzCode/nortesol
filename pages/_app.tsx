b2e3a6d8473111820cdce9cee69a265e4863ed94
sharshakap <55374102+sharshakap@users.noreply.github.com>
2021-09-20 23:01:22 -0300

    feat: implementado rutas basada en acceso

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,49 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Layout from "../components/layout/Layout";
 import firebase, { FirebaseContext } from "../firebase";
 import useAutenticacion from "../hooks/useAutenticacion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const usuario = useAutenticacion();
+  const router = useRouter();
+  const [authorized, setAuthorized] = useState(false);
+  function authCheck(url: string) {
+    // redirect to login page if accessing a private page and not logged in
+    const publicPaths = ["/login", "/", "/register"];
+    const path = url.split("?")[0];
+    if (!usuario && !publicPaths.includes(path)) {
+      setAuthorized(false);
+      router.push({
+        pathname: "/login",
+        query: { returnUrl: router.asPath },
+      });
+    } else {
+      setAuthorized(true);
+    }
+  }
+  useEffect(() => {
+    // run auth check on initial load
+    authCheck(router.asPath);
+
+    // set authorized to false to hide page content while changing routes
+    const hideContent = () => setAuthorized(false);
+    router.events.on("routeChangeStart", hideContent);
+
+    // run auth check on route change
+    router.events.on("routeChangeComplete", authCheck);
+
+    // unsubscribe from events in useEffect return function
+    return () => {
+      router.events.off("routeChangeStart", hideContent);
+      router.events.off("routeChangeComplete", authCheck);
+    };
+  }, [usuario]);
+
   return (
     <>
       <Head>
@@ -38,11 +74,13 @@ function MyApp({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
         />
       </Head>
-      <FirebaseContext.Provider value={{ usuario, firebase }}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </FirebaseContext.Provider>
+      {authorized && (
+        <FirebaseContext.Provider value={{ usuario, firebase }}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </FirebaseContext.Provider>
+      )}
     </>
   );
 }
