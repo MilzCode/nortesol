672921338e446ae535956233dba89d12c62a4e20
sharshakap <55374102+sharshakap@users.noreply.github.com>
2021-11-09 17:10:10 -0300

    fix: se modifico el 404 para que en vez de mostrar error solo redirija al home.

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,19 +2,24 @@ import React from "react";
 import BotonFAColores1 from "../components/general/BotonFAColores1";
 import Link from "next/link";
 
-const RutaDefault = () => {
-  return (
-    <div className="rutaDefault">
-      <p className="rutaDefault__msg NOSELECT">
-        404 D: ! Esta no es una ruta valida amig@
-      </p>
-      <Link href="/" passHref>
-        <div>
-          <BotonFAColores1>ir a la pagina principal</BotonFAColores1>
-        </div>
-      </Link>
-    </div>
-  );
-};
+// const RutaDefault = () => {
+//   return (
+//     <div className="rutaDefault">
+//       <p className="rutaDefault__msg NOSELECT">
+//         404 D: ! Esta no es una ruta valida amig@
+//       </p>
+//       <Link href="/" passHref>
+//         <div>
+//           <BotonFAColores1>ir a la pagina principal</BotonFAColores1>
+//         </div>
+//       </Link>
+//     </div>
+//   );
+// };
+
+/*
+  De esta forma en caso de error mandara al login
+*/
+const RutaDefault = () => window.location.replace("/");
 
 export default RutaDefault;
