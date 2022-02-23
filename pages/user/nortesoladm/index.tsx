d4e5dcaf50163ac6c038e5ed0d5765c4034a5565
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-02-23 19:16:13 -0300

    fix: se puso estilo en panel administrador

--- pages/user/nortesoladm/index.tsx
+++ pages/user/nortesoladm/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Volver from '../../../components/general/Volver';
 import { paths } from '../../../utils/constantes';
 import Wredirect from '../../../helpers/Wredirect';
+import BotonFAColores1 from '../../../components/general/BotonFAColores1';
 
 const nortesoladm = ({ auth, me }: any) => {
 	if (!auth || !me.admin) {
@@ -12,34 +13,54 @@ const nortesoladm = ({ auth, me }: any) => {
 	return (
 		<>
 			<Volver />
-			<br />
-			<Link href="/user/nortesoladm/addproduct" passHref>
-				Crear Producto
-			</Link>
-			<br />
-			<Link href="/user/nortesoladm/addmarca" passHref>
-				Crear Marcas
-			</Link>
-			<br />
-			<Link href="/user/nortesoladm/addcategoria" passHref>
-				Crear Categorias
-			</Link>
-			<br />
-			<Link href="/user/nortesoladm/addportada" passHref>
-				Crear Portada
-			</Link>
-			<br />
-			<Link href="/user/nortesoladm/removeportada" passHref>
-				Remover Portada
-			</Link>
-			<br />
-			<Link href="/user/nortesoladm/addanuncio" passHref>
-				Anuncio
-			</Link>
-			<br />
-			<Link href={paths.searchDesabilitados} passHref>
-				Ver Productos desabilitados
-			</Link>
+			<div>
+				<br />
+				<Link href="/user/nortesoladm/addproduct" passHref>
+					<BotonFAColores1 backgroundColor="#48d597">
+						Crear Producto
+					</BotonFAColores1>
+				</Link>
+				<br />
+				<br />
+				<Link href="/user/nortesoladm/addportada" passHref>
+					<BotonFAColores1 backgroundColor="#005eb8">
+						Crear Portada
+					</BotonFAColores1>
+				</Link>
+				<br />
+				<br />
+				<Link href="/user/nortesoladm/removeportada" passHref>
+					<BotonFAColores1 backgroundColor="#00a5df">
+						Remover Portada
+					</BotonFAColores1>
+				</Link>
+				<br />
+				<br />
+				<Link href="/user/nortesoladm/addanuncio" passHref>
+					<BotonFAColores1 backgroundColor="#f9e547">Anuncio</BotonFAColores1>
+				</Link>
+				<br />
+				<br />
+				<Link href={paths.searchDesabilitados} passHref>
+					<BotonFAColores1 backgroundColor="#f9423a">
+						Ver Productos desabilitados
+					</BotonFAColores1>
+				</Link>
+				<br />
+				<br />
+				<Link href="/user/nortesoladm/addmarca" passHref>
+					<BotonFAColores1 backgroundColor="#ff6a39">
+						Crear Marcas
+					</BotonFAColores1>
+				</Link>
+				<br />
+				<br />
+				<Link href="/user/nortesoladm/addcategoria" passHref>
+					<BotonFAColores1 backgroundColor="#ea5c90">
+						Crear Categorias
+					</BotonFAColores1>
+				</Link>
+			</div>
 		</>
 	);
 };
