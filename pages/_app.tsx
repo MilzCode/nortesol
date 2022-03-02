25670d458f5ba9e4b6389771c61a7212123f31a5
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-03-02 17:58:36 -0300

    fix: performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -85,22 +85,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 					<meta name="description" content="NorteSol" />
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<meta charSet="UTF-8" />
-					<link rel="icon" href="/nortesol.ico" />
-
-					<link
-						rel="stylesheet"
-						href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-					/>
-					<link
-						rel="stylesheet"
-						type="text/css"
-						href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-					/>
-					<link
-						rel="stylesheet"
-						type="text/css"
-						href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-					/>
 				</Head>
 
 				{/* 
