a77dac75d31e4731127bf3ac9805c4ed56462bbd
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-03-02 18:27:18 -0300

    fix: detalles

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhWx_mhj-vrdoki4yjFcqE00xX7wwKhPE",
+  authDomain: "nortesol-92807.firebaseapp.com",
+  projectId: "nortesol-92807",
+  storageBucket: "nortesol-92807.appspot.com",
+  messagingSenderId: "167331895801",
+  appId: "1:167331895801:web:b9f52474120492a3098528",
+  measurementId: "G-Q2XZCD3PN5",
+};
+
+export default firebaseConfig;
