b9943572ceccaac2a385d180139cc485512be788
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-04-23 16:58:53 -0400

    fix: wredirect

--- helpers/Wredirect.tsx
+++ helpers/Wredirect.tsx
@@ -2,15 +2,14 @@
  * Wredirect es un metodo de redireccion que asegura que el objeto window exista.
  */
 const Wredirect = (url = '/', href = false) => {
-	// if (typeof window !== 'undefined') {
-	// 	if (href) {
-	// 		window.location.href = url;
-	// 	} else {
-	// 		window.location.replace(url);
-	// 	}
-	// }
-	// return null;
-	console.log("acaaa")
+	if (typeof window !== 'undefined') {
+		if (href) {
+			window.location.href = url;
+		} else {
+			window.location.replace(url);
+		}
+	}
+	return null;
 };
 
 export default Wredirect;
